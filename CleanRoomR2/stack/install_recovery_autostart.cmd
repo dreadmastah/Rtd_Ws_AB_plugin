8aa25d09beff16dae6896005739747c21812e738
Makoy <makoymakoymakoy3@gmail.com>
2026-09-19 20:03:12 +0800

    Add R2.1 recovery install_recovery_autostart.cmd

---
+++ CleanRoomR2/stack/install_recovery_autostart.cmd
@@ -0,0 +1,11 @@
+@echo off
+setlocal EnableExtensions
+cd /d "%~dp0"
+set "DBNAME=%~1"
+if "%DBNAME%"=="" set "DBNAME=WSRTD"
+if not exist ".venv\Scripts\python.exe" (
+  call install_wsrtd_stack.cmd
+  if errorlevel 1 exit /b 1
+)
+".venv\Scripts\python.exe" autostart_manager.py --install --dbname "%DBNAME%"
+exit /b %errorlevel%
