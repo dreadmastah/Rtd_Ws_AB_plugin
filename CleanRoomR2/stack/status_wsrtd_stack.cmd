3ca6c1ec4662b8609788bdd77372562c5287a801
Makoy <makoymakoymakoy3@gmail.com>
2026-09-19 19:16:11 +0800

    Add CleanRoomR2/stack/status_wsrtd_stack.cmd

---
+++ CleanRoomR2/stack/status_wsrtd_stack.cmd
@@ -0,0 +1,8 @@
+@echo off
+setlocal
+cd /d "%~dp0"
+if not exist ".venv\Scripts\python.exe" (
+  echo WSRTD_STACK_STATUS=NO_VENV
+  exit /b 1
+)
+".venv\Scripts\python.exe" stack_launcher.py --status
