cfe47e2a5f3684c3519950f79973bb1a572640f4
Makoy <makoymakoymakoy3@gmail.com>
2026-09-20 09:36:52 +0800

    Add local simulation reconciliation pipe name

--- Core/ipc/include/astu/ipc/named_pipe.hpp
+++ Core/ipc/include/astu/ipc/named_pipe.hpp
@@ -20,6 +20,8 @@
 namespace astu::ipc {
 
 inline constexpr wchar_t kExecutionPipeName[] = L"\\\\.\\pipe\\AstuExecutionSim.v1";
+inline constexpr wchar_t kReconciliationPipeName[] =
+    L"\\\\.\\pipe\\AstuExecutionReconcileSim.v1";
 
 #ifdef _WIN32
 
